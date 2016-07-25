bd51be3bead28c238069d393da449e4edbee1b56
Quant911 <quant911@github.com>
2016-07-25 16:18:46 -0400

    first complete version. 1) results reconciled with Excel; 2) matplotlib figure no show in PyCharm; 3) profiling code in- but need much bigger dataset to vet.

--- PositionSizing.py
+++ PositionSizing.py
@@ -1,69 +1,122 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import datetime
 import functools as ft
+from collections import Callable
 
-def cumRet(x, y):
+
+def cum_ret(x, y):
     return x * (1+y)
 
-def foldCumRet(x):
-    return ft.reduce(cumRet, x, 10000.0)
 
-if __name__ == '__main__':
-    data = pd.read_csv('d:/dev/tinyp/PositionSizing/Excel/spx.csv', dtype={'Date': datetime.date, 'SPX': np.float64},
+def reduce_cum_ret(y):
+    return ft.reduce(cum_ret, y, 10000.0)
+
+
+class SmAvg(Callable):
+    def __init__(self, c):
+        self.c_ = c
+
+    def __call__(self, x, y):
+        return self.c_*y+(1-self.c_)*x
+
+
+class ReduceSmAvg(Callable):
+    def __init__(self, xx, f):
+        self.xx_ = xx
+        self.f_ = f
+
+    def __call__(self, y):
+        return ft.reduce(self.f_, y, self.xx_)
+
+
+def create_features(fn):
+    data = pd.read_csv(fn, dtype={'Date': datetime.date, 'SPX': np.float64},
                        index_col='Date',
                        infer_datetime_format=True)  # Date, SPX
-    data['Performance'] = data['SPX']/ data['SPX'].shift(1)  - 1.0  # how to specify type?
-    data.set_value('01/02/2008', 'Performance', 0.0)
-    print(data.head())
-    print(data.dtypes)
 
-    data['N10000'] = data['Performance'].expanding().apply(foldCumRet)
+    print('after CSV loading, now={0}'.format(datetime.datetime.now()))
+    data['Performance'] = data['SPX'] / data['SPX'].shift(1) - 1.0  # how to specify type?
+    data.set_value('01/02/2008', 'Performance', 0.0)
+    # print(data.head())
+    # print(data.dtypes)
+
+    data['N10000'] = data['Performance'].expanding().apply(reduce_cum_ret)
     data['CumReturns'] = data['N10000'] / 10000.0 - 1.0
 
-    data['SMA_5'] = data['SPX'].rolling(window=5).mean() # pd.rolling_mean(data['SPX'], 5)[4:]
-    print(data.head(10))
+    data['SMA_5'] = data['SPX'].rolling(window=5).mean()  # pd.rolling_mean(data['SPX'], 5)[4:]
 
     data['SMA_25'] = data['SPX'].rolling(window=25).mean()
     data['Change'] = data['SPX'] - data['SPX'].shift(1)
     data['Change'].fillna(0)
     data.set_value('01/02/2008', 'Change', 0.0)
-    # try data.ix(data.Date='01/02/2008', 'Change') = 0.0
-    print(data.head(40))
-    idx_up = data['Change'] >0
-    # data['Move_Up'] = data['Change'].map(lambda c: np.floor(c, 0.0)) # np.floor(data['Change'], 0)
-    # !!!!
+
     data['Move_Up'] = data['Change'].copy()
-    data.loc[data.Change<0.0, 'Move_Up'] = 0.0 # todo: can we combine deep-copy-n-modify into one statement
+    data.loc[data.Change < 0.0, 'Move_Up'] = 0.0  # todo: can we combine deep-copy-n-modify into one statement
 
     data['Move_Down'] = -data['Change']
-    data.loc[data.Move_Down <0.0, 'Move_Down'] = 0.0
+    data.loc[data.Move_Down < 0.0, 'Move_Down'] = 0.0
 
     data['Avg_Up'] = data['Move_Up'].rolling(14).mean().fillna(0.0)
     data['Avg_Down'] = data['Move_Down'].rolling(14).mean().fillna(0.0)
 
-    const_p1 = 1.0/14.0
-    data['Smoothed_Avg_Up'] = data['Move_Up'] * const_p1 + data['Avg_Up'].shift(1) * (1.0 - const_p1) # we need fold and access two cols!
-    v01 = data.at['01/23/2008', 'Move_Up']
-    v02 = data.at['01/22/2008', 'Avg_Up']
-    v0 =  v01 * const_p1 +  v02 * (1.0 - const_p1)
-    print('MoveUp={0}, AvgUp={1}, Sm={2}'.format(v01, v02, v0))
-    # v0 = data.ix('01/22/2008', 'Move_Up') * const_p1 + data.get_value('01/18/2008', 'Avg_Up') * (1.0 - const_p1)
-    data['Smoothed_Avg_Down'] = data['Move_Down'] * const_p1 + data['Avg_Down'].shift(1) * (1.0 - const_p1)
+    const_p1 = 1.0 / 14.0
+    sm_offset = 14
+    data['Smoothed_Avg_Up'] = np.zeros(data.shape[0])
+    data_move_up = data['Move_Up']
+    data['Smoothed_Avg_Up'][sm_offset:] = data_move_up[sm_offset:].expanding().apply(
+        ReduceSmAvg(data.at['01/22/2008', 'Avg_Up'], SmAvg(const_p1)))
+
+    data['Smoothed_Avg_Down'] = np.zeros(data.shape[0])
+    data_move_dn = data['Move_Down']
+    data['Smoothed_Avg_Down'][sm_offset:] = data_move_dn[sm_offset:].expanding().apply(
+        ReduceSmAvg(data.at['01/22/2008', 'Avg_Down'], SmAvg(const_p1)))
     data['RS'] = data['Smoothed_Avg_Up'] / data['Smoothed_Avg_Down']
+    data['RS'].fillna(0.0)
     data['RSI_14'] = 100.0 - 100.0 / (1.0 + data['RS'])
-    data['Signal'] = np.select([ data['SMA_5']>data['SMA_25']], [1], default=-1)
 
-    data['Position_Sizing_0_30'] = np.where(data['RSI_14'].shift(1) < 30, 0.3, 0)
-#    data['Position_Sizing_30_50'] = np.where(data['RSI_14'].shift(1) > 30 & data['RSI_14'].shift(1) < 50, 1, 0)
-    data['Position_Sizing_30_50'] = data['Position_Sizing_0_30'].copy() # fake!!!
-    data['Position_Sizing_50_70'] = data['Position_Sizing_0_30'].copy()
-    data['Position_Sizing_70_100'] = data['Position_Sizing_0_30'].copy()
+    # implement shift-1-day logic into feature
+    # featureData = pd.concat([data['SMA_5'], data['SMA_25'], data['RSI_14']], axis= 1)
 
-    data['Position_Sizing_sum'] = data['Position_Sizing_0_30'] + data['Position_Sizing_30_50'] + data['Position_Sizing_50_70'] + data['Position_Sizing_70_100']
+    out = data[['SMA_5', 'SMA_25', 'RSI_14']]
 
-    #data['Move_Up'] = [ np.floor(c, 0.0) for c in data['Change'] ]
-    # data['Move_Down'] = data['Change'].apply(lambda c: -np.ceil(c, 0.0) ) # np.fabs(np.ceil(data['Change'], 0))
-    # data['Move_Up'] = data[data['Change'] > 0].fillna(0)
+    out.shift(1)
+    return out, data['Performance']
 
-    print(data.head(40))
+
+if __name__ == '__main__':
+    fn = 'd:/dev/tinyp/PositionSizing/Excel/spx.csv'
+    df, perf_series = create_features(fn)
+
+    df['Signal'] = np.select([df['SMA_5'] > df['SMA_25']], [1], default=-1)
+
+    df['Signal'][:25] = 0
+    df['01/02/2008':'01/22/2008']['RSI_14'] = 0
+    rsi = df['RSI_14']
+    df['S_0_30'] = np.where(df['RSI_14'] < 30, 0.3, 0)
+    df['S_30_50'] = np.where( (rsi < 50) & (rsi > 30), 1, 0)  # somehow 30 < rsi < 50 condition will throw
+    df['S_50_70'] = np.where( (rsi < 70) & (rsi > 50), 1, 0)
+    df['S_70_100'] = np.where(70 < rsi, 0.3, 0)
+
+    df['S_sum'] = df['S_0_30'] + df['S_30_50'] + df['S_50_70'] + df['S_70_100']
+
+    df['Performance'] = perf_series
+    df['Strat_Daily_Return'] = df['Performance'] * df['Signal'] * df['S_sum']
+    df['Strat_N10000'] = df['Strat_Daily_Return'].expanding().apply(reduce_cum_ret)
+    df['Strat_CumReturns'] = df['Strat_N10000'] / 10000.0 - 1.0
+    print('after running strat, now={0}'.format(datetime.datetime.now()))
+    pd.set_option('display.max_columns', 100)
+    pd.set_option('display.expand_frame_repr', False)
+
+    print('Head')
+    print(df.head(40))
+    print('Tail')
+    print(df.tail(20))
+
+    input('Press any key to see graph')
+
+    plt.figure()
+    df[['Performance', 'Strat_CumReturns']].plot()
+    plt.interactive(False)
+    plt.show()
